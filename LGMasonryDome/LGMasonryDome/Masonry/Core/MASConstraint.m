c471d92c98fcf981173cb3c92095d50d1279b455
liguangshuai <guangshuai.li@591hx.com>
2019-01-08 12:17:28 +0800

    ad d
    ad d

--- LGMasonryDome/LGMasonryDome/Masonry/Core/MASConstraint.m
+++ LGMasonryDome/LGMasonryDome/Masonry/Core/MASConstraint.m
@@ -23,7 +23,6 @@
 }
 
 #pragma mark - NSLayoutRelation proxies
-
 - (MASConstraint * (^)(id))equalTo {
     return ^id(id attribute) {///第一个id是回调的返回值；第二个id是回调的参数。
         return self.equalToWithRelation(attribute, NSLayoutRelationEqual);
@@ -31,51 +30,48 @@
 }
 
 - (MASConstraint * (^)(id))mas_equalTo {
-    return ^id(id attribute) {
+    return ^id(id attribute) {///第一个id是回调的返回值；第二个id是回调的参数。
         return self.equalToWithRelation(attribute, NSLayoutRelationEqual);
     };
 }
 
 - (MASConstraint * (^)(id))greaterThanOrEqualTo {
-    return ^id(id attribute) {
+    return ^id(id attribute) {///第一个id是回调的返回值；第二个id是回调的参数。
         return self.equalToWithRelation(attribute, NSLayoutRelationGreaterThanOrEqual);
     };
 }
 
 - (MASConstraint * (^)(id))mas_greaterThanOrEqualTo {
-    return ^id(id attribute) {
+    return ^id(id attribute) {///第一个id是回调的返回值；第二个id是回调的参数。
         return self.equalToWithRelation(attribute, NSLayoutRelationGreaterThanOrEqual);
     };
 }
 
 - (MASConstraint * (^)(id))lessThanOrEqualTo {
-    return ^id(id attribute) {
+    return ^id(id attribute) {///第一个id是回调的返回值；第二个id是回调的参数。
         return self.equalToWithRelation(attribute, NSLayoutRelationLessThanOrEqual);
     };
 }
 
 - (MASConstraint * (^)(id))mas_lessThanOrEqualTo {
-    return ^id(id attribute) {
+    return ^id(id attribute) {///第一个id是回调的返回值；第二个id是回调的参数。
         return self.equalToWithRelation(attribute, NSLayoutRelationLessThanOrEqual);
     };
 }
 
 #pragma mark - MASLayoutPriority proxies
-
 - (MASConstraint * (^)(void))priorityLow {
     return ^id{
         self.priority(MASLayoutPriorityDefaultLow);
         return self;
     };
 }
-
 - (MASConstraint * (^)(void))priorityMedium {
     return ^id{
         self.priority(MASLayoutPriorityDefaultMedium);
         return self;
     };
 }
-
 - (MASConstraint * (^)(void))priorityHigh {
     return ^id{
         self.priority(MASLayoutPriorityDefaultHigh);
@@ -84,42 +80,36 @@
 }
 
 #pragma mark - NSLayoutConstraint constant proxies
-
 - (MASConstraint * (^)(MASEdgeInsets))insets {
     return ^id(MASEdgeInsets insets){
         self.insets = insets;
         return self;
     };
 }
-
 - (MASConstraint * (^)(CGFloat))inset {
     return ^id(CGFloat inset){
         self.inset = inset;
         return self;
     };
 }
-
 - (MASConstraint * (^)(CGSize))sizeOffset {
     return ^id(CGSize offset) {
         self.sizeOffset = offset;
         return self;
     };
 }
-
 - (MASConstraint * (^)(CGPoint))centerOffset {
     return ^id(CGPoint offset) {
         self.centerOffset = offset;
         return self;
     };
 }
-
 - (MASConstraint * (^)(CGFloat))offset {
     return ^id(CGFloat offset){
         self.offset = offset;
         return self;
     };
 }
-
 - (MASConstraint * (^)(NSValue *value))valueOffset {
     return ^id(NSValue *offset) {
         NSAssert([offset isKindOfClass:NSValue.class], @"expected an NSValue offset, got: %@", offset);
@@ -127,7 +117,6 @@
         return self;
     };
 }
-
 - (MASConstraint * (^)(id offset))mas_offset {
     // Will never be called due to macro
     return nil;
@@ -156,65 +145,50 @@
 }
 
 #pragma mark - Semantic properties
-
 - (MASConstraint *)with {
     return self;
 }
-
 - (MASConstraint *)and {
     return self;
 }
 
 #pragma mark - Chaining
-
 - (MASConstraint *)addConstraintWithLayoutAttribute:(NSLayoutAttribute __unused)layoutAttribute {
     MASMethodNotImplemented();
 }
-
 - (MASConstraint *)left {
     return [self addConstraintWithLayoutAttribute:NSLayoutAttributeLeft];
 }
-
 - (MASConstraint *)top {
     return [self addConstraintWithLayoutAttribute:NSLayoutAttributeTop];
 }
-
 - (MASConstraint *)right {
     return [self addConstraintWithLayoutAttribute:NSLayoutAttributeRight];
 }
-
 - (MASConstraint *)bottom {
     return [self addConstraintWithLayoutAttribute:NSLayoutAttributeBottom];
 }
-
 - (MASConstraint *)leading {
     return [self addConstraintWithLayoutAttribute:NSLayoutAttributeLeading];
 }
-
 - (MASConstraint *)trailing {
     return [self addConstraintWithLayoutAttribute:NSLayoutAttributeTrailing];
 }
-
 - (MASConstraint *)width {
     return [self addConstraintWithLayoutAttribute:NSLayoutAttributeWidth];
 }
-
 - (MASConstraint *)height {
     return [self addConstraintWithLayoutAttribute:NSLayoutAttributeHeight];
 }
-
 - (MASConstraint *)centerX {
     return [self addConstraintWithLayoutAttribute:NSLayoutAttributeCenterX];
 }
-
 - (MASConstraint *)centerY {
     return [self addConstraintWithLayoutAttribute:NSLayoutAttributeCenterY];
 }
-
 - (MASConstraint *)baseline {
     return [self addConstraintWithLayoutAttribute:NSLayoutAttributeBaseline];
 }
-
 - (MASConstraint *)firstBaseline {
     return [self addConstraintWithLayoutAttribute:NSLayoutAttributeFirstBaseline];
 }
